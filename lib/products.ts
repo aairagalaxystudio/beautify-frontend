ed7c33f45a9b04e104f8041fdbd783da690009c1
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-20 10:47:10 +0530

    Create lib/products.ts

---
+++ lib/products.ts
@@ -0,0 +1,14 @@
+export const products = [
+  {
+    slug: 'royal-nude',
+    name: 'Royal Nude',
+    price: 1499,
+    description: 'Soft luxury nude finish.',
+  },
+  {
+    slug: 'french-classic',
+    name: 'French Classic',
+    price: 1299,
+    description: 'Timeless elegance.',
+  },
+]
