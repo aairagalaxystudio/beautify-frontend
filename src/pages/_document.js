27c68d7f14f1946efab84ba63402b73359f80e84
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-23 22:29:36 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,21 +4,27 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+
         {/* Luxury Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600&family=Inter:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
 
-        {/* Meta */}
+        {/* Theme */}
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Beautify by Mansi - Luxury Press On Nails" />
+
+        {/* SEO */}
+        <meta
+          name="description"
+          content="Beautify by Mansi – Luxury Press On Nails"
+        />
 
         {/* Favicon */}
         <link rel="icon" href="/logo.svg" />
+
       </Head>
 
       <body>
