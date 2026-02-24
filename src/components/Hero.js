ba8f7e281b14de37d743129b326350002cdfc6b9
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-24 22:29:20 +0530

    Create src/components/Hero.js

---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+export default function Hero(){
+
+return(
+
+<section className="hero">
+
+<div>
+
+<h1>Luxury Press-On Nails</h1>
+
+<p>Designed for elegance</p>
+
+<a href="/collection" className="btn">
+Explore Collection
+</a>
+
+</div>
+
+</section>
+
+)
+
+}
