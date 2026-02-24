5d8a8a669ff9b5d759fb0a7c301ce2782bbd35e9
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 01:05:35 +0530

    Create pages/_document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Beautify by Mansi — Luxury Press On Nails</title>
+        <meta
+          name="description"
+          content="Premium handcrafted press-on nails inspired by Dior elegance."
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
