99e46dfc12d1ec38e9db0bf0bfda9d2f46c204c3
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 00:25:27 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,9 @@
 export default function Hero(){
-
 return(
-
 <section className="hero">
-
-<div>
-
 <h1>Luxury Press-On Nails</h1>
-
-<p>Designed for elegance</p>
-
-<a href="/collection" className="btn">
-Explore Collection
-</a>
-
-</div>
-
+<p>Premium handmade designs</p>
+<button className="btn">Shop Collection</button>
 </section>
-
 )
-
 }
