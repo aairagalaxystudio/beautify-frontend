8731e483ca001e21fe489b89f70be00f8fb7b331
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-24 22:00:20 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,29 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
 
-        {/* Luxury Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600&family=Inter:wght@300;400;500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600&family=Inter:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
 
-        {/* Theme */}
         <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Beautify by Mansi Luxury Beauty Brand" />
 
-        {/* SEO */}
-        <meta
-          name="description"
-          content="Beautify by Mansi – Luxury Press On Nails"
-        />
-
-        {/* Favicon */}
-        <link rel="icon" href="/logo.svg" />
+        <link rel="icon" href="/logo.svg"/>
 
       </Head>
 
@@ -32,5 +25,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
