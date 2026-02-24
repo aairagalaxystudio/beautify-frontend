6ba186bf858086e3d51c1d9e488ee7d1f5ae5db1
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-24 21:59:25 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,5 @@
-```javascript
-import '@/styles/globals.css';
-import { Playfair_Display, Inter } from 'next/font/google';
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-primary',
-});
-
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['300', '400', '500'],
-  variable: '--font-body',
-});
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }) {
-  return (
-    <main className={`${playfair.variable} ${inter.variable}`}>
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />
 }
-```
