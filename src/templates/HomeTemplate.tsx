47c7baca1dcf598e5be2ef1fa43e8cd7cdaf2f1e
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 08:33:50 +0530

    Create src/templates/HomeTemplate.tsx

---
+++ src/templates/HomeTemplate.tsx
@@ -0,0 +1,15 @@
+```tsx
+import HeroVideo from '@/components/sections/HeroVideo';
+import FeaturedProducts from '@/components/sections/FeaturedProducts';
+import BrandStory from '@/components/sections/BrandStory';
+
+export default function HomeTemplate() {
+  return (
+    <>
+      <HeroVideo />
+      <FeaturedProducts />
+      <BrandStory />
+    </>
+  );
+}
+```
