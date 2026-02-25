78948eaa7e5bbf9693fca635f1c0f9ca0e6d8f42
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 08:28:36 +0530

    Create src/components/sections/BrandStory.tsx

---
+++ src/components/sections/BrandStory.tsx
@@ -0,0 +1,12 @@
+```tsx
+export default function BrandStory() {
+  return (
+    <section className="story">
+      <h2>The Story</h2>
+      <p>Beautify by Mansi is inspired by couture beauty and timeless elegance.</p>
+    </section>
+  );
+}
+```
+
+---
