3521b34bd5fb92f5343ab4d3bdb1360a06e368c9
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 09:09:00 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [], // add your image domains if using external images
+  },
+};
+
+module.exports = nextConfig;
+```
+
+---
