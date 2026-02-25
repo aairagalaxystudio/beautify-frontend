1a8b406dc9eb96343e15e7d2138cad526ee66fa1
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 08:24:05 +0530

    Create src/components/ui/Container.tsx

---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,7 @@
+```tsx
+import { ReactNode } from 'react';
+
+export default function Container({ children }: { children: ReactNode }) {
+  return <div className="container">{children}</div>;
+}
+```
