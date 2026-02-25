407e2df0ad524e9c6d057b2191c4ab76f73c4746
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 08:26:52 +0530

    Create src/components/layout/Layout.tsx

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,17 @@
+```tsx
+import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+```
+
+---
