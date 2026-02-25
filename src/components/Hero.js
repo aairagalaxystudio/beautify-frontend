5250dbb89ce38bd4330e02be3529ed19da92bb42
aairagalaxystudio <aairagalaxystudio@gmail.com>
2026-02-25 08:16:32 +0530

    Delete Hero.js

--- src/components/Hero.js
+++
@@ -1,9 +0,0 @@
-export default function Hero(){
-return(
-<section className="hero">
-<h1>Luxury Press-On Nails</h1>
-<p>Premium handmade designs</p>
-<button className="btn">Shop Collection</button>
-</section>
-)
-}
